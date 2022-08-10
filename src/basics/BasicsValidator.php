dbba76952bfa140e66f0f1ce355597ef77b37ec0
Tanya Fedoriv <tanya.fedoriv25@gmail.com>
2022-08-10 18:02:04 +0300

    Added annotations to classes Basics and BasicsValidator

--- src/basics/BasicsValidator.php
+++ src/basics/BasicsValidator.php
@@ -5,24 +5,39 @@ namespace basics;
 class BasicsValidator implements BasicsValidatorInterface
 {
 
+    /**
+     * @param int $minute
+     * @throws \InvalidArgumentException
+     * @return void
+     */
     public function isMinutesException(int $minute): void
     {
         if ($minute < 0 || $minute > 60) {
-            throw new \InvalidArgumentException("The function only accepts minutes in range from 0 to 60. The minute was ".$minute);
+            throw new \InvalidArgumentException('The function only accepts minutes in range from 0 to 60. The minute was ' . $minute);
         }
     }
 
+    /**
+     * @param int $year
+     * @throws \InvalidArgumentException
+     * @return void
+     */
     public function isYearException(int $year): void
     {
         if ($year < 1900) {
-            throw new \InvalidArgumentException("The function only accepts years that is bigger than 1900. The year was ".$year);
+            throw new \InvalidArgumentException('The function only accepts years that is bigger than 1900. The year was ' . $year);
         }
     }
 
+    /**
+     * @param string $input
+     * @throws \InvalidArgumentException
+     * @return void
+     */
     public function isValidStringException(string $input): void
     {
         if (strlen($input) != 6) {
-            throw new \InvalidArgumentException("The function only accept 6 digits");
+            throw new \InvalidArgumentException('The function only accept 6 digits');
         }
     }
 }
